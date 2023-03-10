b10f1b254783737c70e2654c62d9427ffb519934
Robin Schatzl <tmai02476@gmail.com>
2023-03-10 14:42:32 +0100

    added Test addNewOrderfail

--- src/test/java/OrderRepoTest.java
+++ src/test/java/OrderRepoTest.java
@@ -26,4 +26,23 @@ class OrderRepoTest {
 
     }
 
+    @Test
+    void addNewOrderFail() {
+        //GIVEN
+        Map<String, Order> orderTestMap = new HashMap<>();
+        Order order1 = new Order("Or01", Map.of(
+                "B10", new Product("B10", "Becher")
+        ));
+        OrderRepo orderRepo1 = new OrderRepo(orderTestMap);
+        //WHEN
+        orderRepo1.add(order1);
+        Map<String, Order> actual = orderTestMap;
+        Map<String, Order> expectedMap = new HashMap<>(Map.of("Or02", order1));
+
+        //THEN
+        assertNotEquals(expectedMap, actual);
+
+
+    }
+
 }
